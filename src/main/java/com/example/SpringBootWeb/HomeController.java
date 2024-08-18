a5093f23455d90f20cec4bfdfbea4202f5824301
saichethanr <saichethan9019@gmail.com>
2024-08-18 10:14:52 +0530

    jsp page converted and seen onto the webpage sucess but sending a request to a controller need to be done

--- src/main/java/com/example/SpringBootWeb/HomeController.java
+++ src/main/java/com/example/SpringBootWeb/HomeController.java
@@ -1,11 +1,15 @@
 package com.example.SpringBootWeb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     //whatever is returned by the below  function should be a view technoloy
     //but every url should match with the method first
+    //by deafult springboot does not support the jsp page we have to convert it into a servlet----addd tomcat jasper in dependency
+    //the request aping method below maps the home apage to  the method
+    @RequestMapping("/")
     public String home(){
         System.out.println("home method called");
         return "index.jsp";
